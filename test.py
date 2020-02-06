c2bfb6623c3f7eb33635334f9f2e8922fdd3c4d8
snitch30 <stejasmunees@gmail.com>
2020-02-06 22:21:20 +0000

    modified_test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ class SanityTest():
 	""" Sanity Test """
 	def test_addition_sanity(self):
 		''' Basic Sanity Test '''
-		assert(1 + 1 == 23)
+		assert(1 + 1 == 2)
 
 SanityVar = SanityTest()
 SanityVar.test_addition_sanity()
